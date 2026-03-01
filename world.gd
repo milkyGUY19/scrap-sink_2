242f2158d21e4c0799fb6ab743af3923c52540df
milkyGUY19 <jaramach08@gmail.com>
2026-03-01 05:38:25 +0100

    corect

---
+++ world.gd
@@ -0,0 +1,82 @@
+extends Node2D
+
+@onready var sea_weed_1: AnimatedSprite2D = $SeaWeed/SeaWeed1
+@onready var sea_weed_2: AnimatedSprite2D = $SeaWeed/SeaWeed2
+@onready var sea_weed_3: AnimatedSprite2D = $SeaWeed/SeaWeed3
+@onready var sea_weed_4: AnimatedSprite2D = $SeaWeed/SeaWeed4
+@onready var sea_weed_5: AnimatedSprite2D = $SeaWeed/SeaWeed5
+@onready var sea_weed_6: AnimatedSprite2D = $SeaWeed/SeaWeed6
+@onready var sea_weed_15: AnimatedSprite2D = $SeaWeed/SeaWeed15
+@onready var sea_weed_16: AnimatedSprite2D = $SeaWeed/SeaWeed16
+@onready var sea_weed_17: AnimatedSprite2D = $SeaWeed/SeaWeed17
+@onready var sea_weed_18: AnimatedSprite2D = $SeaWeed/SeaWeed18
+@onready var sea_weed_7: AnimatedSprite2D = $SeaWeed/SeaWeed7
+@onready var sea_weed_8: AnimatedSprite2D = $SeaWeed/SeaWeed8
+@onready var sea_weed_9: AnimatedSprite2D = $SeaWeed/SeaWeed9
+@onready var sea_weed_10: AnimatedSprite2D = $SeaWeed/SeaWeed10
+@onready var sea_weed_19: AnimatedSprite2D = $SeaWeed/SeaWeed19
+@onready var sea_weed_20: AnimatedSprite2D = $SeaWeed/SeaWeed20
+@onready var sea_weed_11: AnimatedSprite2D = $SeaWeed/SeaWeed11
+@onready var sea_weed_12: AnimatedSprite2D = $SeaWeed/SeaWeed12
+@onready var sea_weed_13: AnimatedSprite2D = $SeaWeed/SeaWeed13
+@onready var sea_weed_14: AnimatedSprite2D = $SeaWeed/SeaWeed14
+@onready var sea_weed_22: AnimatedSprite2D = $SeaWeed/SeaWeed22
+@onready var sea_weed_23: AnimatedSprite2D = $SeaWeed/SeaWeed23
+@onready var sea_weed_24: AnimatedSprite2D = $SeaWeed/SeaWeed24
+@onready var sea_weed_25: AnimatedSprite2D = $SeaWeed/SeaWeed25
+@onready var sea_weed_21: AnimatedSprite2D = $SeaWeed/SeaWeed21
+@onready var sea_weed_26: AnimatedSprite2D = $SeaWeed/SeaWeed26
+@onready var sea_weed_27: AnimatedSprite2D = $SeaWeed/SeaWeed27
+@onready var sea_weed_28: AnimatedSprite2D = $SeaWeed/SeaWeed28
+@onready var sea_weed_29: AnimatedSprite2D = $SeaWeed/SeaWeed29
+@onready var sea_weed_30: AnimatedSprite2D = $SeaWeed/SeaWeed30
+@onready var sea_weed_31: AnimatedSprite2D = $SeaWeed/SeaWeed31
+@onready var sea_weed_32: AnimatedSprite2D = $SeaWeed/SeaWeed32
+@onready var sea_weed_33: AnimatedSprite2D = $SeaWeed/SeaWeed33
+@onready var sea_weed_34: AnimatedSprite2D = $SeaWeed/SeaWeed34
+@onready var sea_weed_35: AnimatedSprite2D = $SeaWeed/SeaWeed35
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	sea_weed_1.play("default")
+	sea_weed_2.play("default")
+	sea_weed_3.play("default")
+	sea_weed_4.play("default")
+	sea_weed_5.play("default")
+	sea_weed_6.play("default")
+	sea_weed_7.play("default")
+	sea_weed_8.play("default")
+	sea_weed_9.play("default")
+	sea_weed_10.play("default")
+	sea_weed_11.play("default")
+	sea_weed_12.play("default")
+	sea_weed_13.play("default")
+	sea_weed_14.play("default")
+	sea_weed_15.play("default")
+	sea_weed_16.play("default")
+	sea_weed_17.play("default")
+	sea_weed_18.play("default")
+	sea_weed_19.play("default")
+	sea_weed_20.play("default")
+	sea_weed_21.play("default")
+	sea_weed_22.play("default")
+	sea_weed_23.play("default")
+	sea_weed_24.play("default")
+	sea_weed_25.play("default")
+	sea_weed_26.play("default")
+	sea_weed_27.play("default")
+	sea_weed_28.play("default")
+	sea_weed_29.play("default")
+	sea_weed_30.play("default")
+	sea_weed_31.play("default")
+	sea_weed_32.play("default")
+	sea_weed_33.play("default")
+	sea_weed_34.play("default")
+	sea_weed_35.play("default")
+	
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
